0c119804c148f033d94ffa4f9d00893c1e082d20
Александр Ситников <you@example.com>
2023-01-10 15:49:14 +0500

    уточнения и правки

--- 12-luna/app.js
+++ 12-luna/app.js
@@ -2,26 +2,28 @@
 
 // ***** Карта *****
 
-const mapVisa = "4561-2612-1234-5464";
+const mapVisa = "4563-2612-1234-5464";
 
 // ***** Первый вариант *****
 
 function mapValid(string) {
-  const joinNumber = string.split("-").join(""); // убрал все лишнее
-  let evenValue = 0; // четное значение
+  const joinNumber = string.split("-").join(""); 
+  let sumLuna = 0;
   for (let i = 0; i < joinNumber.length; i++) {
-    // беру всю строку с картой и перебираю
+    
     if (i % 2 === 0) {
       if (joinNumber[i] * 2 > 9) {
-        // получаем нечетные числа (46211356) и умножаем на 2
-        evenValue += joinNumber[i] * 2 - 9; // четные числа
+    
+        sumLuna += joinNumber[i] * 2 - 9; // вот тут идет мутация
       } else {
-        evenValue += joinNumber[i] * 2;
+        sumLuna += joinNumber[i] * 2; // и сдесь правильно?
       }
     }
   }
-  return evenValue % 10 === 0;
+  return sumLuna % 10 === 0;
 }
+
+//ЭТО ФУНКЦИЯ ПОЛУЧАЕТСЯ НЕ ЧИСТАЯ!!!
 
 console.log(mapValid(mapVisa));
 
@@ -32,7 +34,7 @@ console.log(mapValid(mapVisa));
  *
  *  2. создал константу splitNumber -
  *
- *  а) копируем slice() сплитом убераю дефисы и конверктирую строку в массив.
+ *  а) сплитом убераю дефисы и конверктирую строку в массив.
  *
  *  б) джойном собираю чистую строку без дефисов.
  *
@@ -42,33 +44,27 @@ console.log(mapValid(mapVisa));
  *
  *  д) Итог получаю чистый массив [4,5,6,1,2,6,1,2,1,2,3,4,5,4,6,4]
  *
- *  3. Использую метод reduce()
- *
- *  а) index = находим по индексу нечетный номера (не цифры в массиве)!
- *
- *  б) и item =  каждый элемент умножаем на два
- *
- *  в) и если item больше 9 то вычитаем из него (9)
- *
- *  г) делаем возврат reduce() т.е складываем все в аккамулятор = получаем (57)
- *
- *  д) делаем возврат функции mapValid1 - константу splitNumber если есть остаток от 10 то функция выдаст false. Если нет остатка то true!
- *
  */
 
 const mapValid1 = (string) => {
   const splitNumber = string
-    .slice()
     .split("-")
     .join("")
     .split("")
     .map(Number)
     .reduce((acc, item, index) => {
-      index % 2 === 0 && (item *= 2);
-      item > 9 && (item -= 9);
-      return acc + item;
+      if (index % 2 === 0) {  // находим нечетные по порядку
+        if (item * 2 > 9) { // каждую цифру не четную если она > 9 возвращаем acc+item *2 - 9
+          return acc + item * 2 - 9;
+        }
+        return acc + item * 2; // все цифры уножаем на 2 и возвращаем
+      } else {
+        return acc + Number(item); // возвращаем все цифры складываем в acc
+      }
     }, 0);
-  return splitNumber % 10 === 0;
+  return splitNumber % 10 === 0; // от общей суммы ищем остаток от 10
 };
 
 console.log(mapValid1(mapVisa));
+
+// ЭТА ФУНКЦИЯ ЧИСТАЯ!!!
