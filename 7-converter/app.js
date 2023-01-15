a443d9a1a1bc14066347517f78757bdcc221487b
Александр Ситников <you@example.com>
2023-01-15 16:37:16 +0500

    правки

--- 7-converter/app.js
+++ 7-converter/app.js
@@ -1,33 +1,31 @@
-'use strict';
+"use strict";
 
 // курсы валют
 
 const Currency = {
-    USD: 0.014,
-    EUR: 0.015,
-    TRY: 0.277,
-    KZT: 6.82,
-    RUB: 1,
-  };
-  // текущая валюта - currentCurrency
-  // желаемая валюта - desiredCurrency
-  // сумма - amount
+  USD: 0.014,
+  EUR: 0.015,
+  TRY: 0.277,
+  KZT: 6.82,
+  RUB: 1,
+};
+// текущая валюта - currentCurrency
+// желаемая валюта - desiredCurrency
+// сумма - amount
 
-    function currencyСonverter (currentCurrency, desiredCurrency, amount) {
-      if (!currentCurrency) {
-        return null;
-      } else if (!desiredCurrency) {
-        return null;
-      }
-        return (currentCurrency * amount)  * desiredCurrency;
+function currencyСonverter(currentCurrency, desiredCurrency, amount) {
+  if (!currentCurrency) {
+    return null;
+  } else if (!desiredCurrency) {
+    return null;
   }
+  return currentCurrency * amount * desiredCurrency;
+}
 
-  currencyСonverter();
+currencyСonverter();
 
 console.log(currencyСonverter(Currency.USD, Currency.KZT, 1000));
 console.log(currencyСonverter(Currency.RUB, Currency.TRY, 1000));
 console.log(currencyСonverter(Currency.TRY, Currency.EUR, 1000));
 console.log(currencyСonverter(Currency.EDR, Currency.EUR, 1000));
 console.log(currencyСonverter(Currency.TRY, Currency.EDR, 1000));
-
-
